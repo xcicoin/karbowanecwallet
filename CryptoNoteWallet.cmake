91ef8bfa0a664d9130be79867f97c2d94dab9360
xcicoin <XCICOIN@hmamail.com>
2016-10-26 20:20:29 +0400

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,5 +1,5 @@
 
-set(CN_PROJECT_NAME "karbowanec")
-set(CN_CURRENCY_DISPLAY_NAME "Карбованець")
-set(CN_CURRENCY_TICKER "KRB")
+set(CN_PROJECT_NAME "xciwallet")
+set(CN_CURRENCY_DISPLAY_NAME "XCI Coin")
+set(CN_CURRENCY_TICKER "XCI")
 set(CN_VERSION 1.0.0)
